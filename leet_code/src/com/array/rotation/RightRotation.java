ec01985a5c94252a383cbe2d82c94b3b2f32942a
kirangond <kirangond05@gmail.com>
2024-02-22 23:02:08 +0530

    solved rotation of array using left side

---
+++ leet_code/src/com/array/rotation/RightRotation.java
@@ -0,0 +1,13 @@
+package com.array.rotation;
+
+public class RightRotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[] = {1,2,3,4,5,6,7}, k=3;
+		
+		//create new temp array of original size
+	}
+
+}
