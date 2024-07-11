046a58dfd6e10e6c891fc674231950dafd12e3f9
kirangond <kirangond05@gmail.com>
2024-07-11 19:19:10 +0530

    reverse a array

---
+++ leet_code/src/com/strings/reverse/ReverseCharArray.java
@@ -0,0 +1,19 @@
+package com.strings.reverse;
+
+
+
+
+
+public class ReverseCharArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      char[] c = {'h','e','l','l','o'};	
+      
+      for(int i=c.length-1;i>=0;i--)
+      {
+    	  System.out.print(c[i]);
+      }
+	}
+
+}
